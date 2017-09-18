b9c218f656780646b67f0b5089155c59d0ae34cf
behzadmonfared <31106305+behzadmonfared@users.noreply.github.com>
2017-09-18 18:20:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var cmd=require('node-cmd');
 var fs=require('fs');
 var validator = require('is-json');
 var unique = require('array-unique');
-
-
 function findproxy(){
       var query = '+":8080" +":3128" +":80" filetype:txt';
       proxyFinder.findProxiesFromSearch(query, 'en', 30, function(host, port) {
@@ -18,36 +16,60 @@ function findproxy(){
       });
 }
 
-findproxy();
 function listcheck(){
  var contents = fs.readFileSync('ip.txt', 'utf8');
  a = contents.split("\n");
  ar =unique(a);
  ar.forEach(function(dataz){
-
-
-
 });
 }
+
 function runc(dataz,webtest){
-  cmd.get(
+    cmd.get(
        'curl '+webtest+' --proxy'+ " "+dataz,
        function (err, data, stderr){
        var result =validator(data);
-      return  check(result,dataz);
+       return  check(result,dataz);
            }
          );
 
 }
 
+function pc(ip,info){
 
+    webtest = "http://ip-api.com/json";
+    var js = info.split("/");
+    if(js[0]=="json"){
+      back = js[1];
+    }else{
+      back = "";
+    }
+  cmd.get(
+     'curl '+webtest+' --proxy ' +" "+ip,
+     function (err, data, stderr){
+     var result =validator(data);
+     if(!back==null){
+       json =  JSON.parse(data);
+       console.log(json.back);
+       return result;
+     }
+     console.log(data);
+
+     return  result;
+         });
+}
 
 function check(result,dataz){
          if(result==true){
            console.log(dataz);
            fs.appendFile('ipvalid2.txt', dataz+"\n", function (err) {
            if (err) throw err;
-          
+
            });
          };
 }
+
+findproxy();
+
+//get proxy info json format
+ //pc("52.174.89.111:80","json");
